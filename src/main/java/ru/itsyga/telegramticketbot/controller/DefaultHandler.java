4d423a4db2410f34adfc19782e9ddc8bbf743bc3
itsyga <chel1997@mail.ru>
2024-08-05 20:10:59 +0500

    feat(controller): add UpdateHandler interface and implement it (MessageHandler and DefaultHandler). Implementation of CoR and Null Object patterns

---
+++ src/main/java/ru/itsyga/telegramticketbot/controller/DefaultHandler.java
@@ -0,0 +1,15 @@
+package ru.itsyga.telegramticketbot.controller;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class DefaultHandler implements UpdateHandler {
+    @Override
+    public void handleUpdate(Update update) {
+
+    }
+
+    @Override
+    public void setNext(UpdateHandler nextHandler) {
+
+    }
+}
---
+++ src/main/java/ru/itsyga/telegramticketbot/controller/MessageHandler.java
@@ -0,0 +1,24 @@
+package ru.itsyga.telegramticketbot.controller;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Order(1)
+@Component
+public class MessageHandler implements UpdateHandler {
+    private UpdateHandler nextHandler;
+
+    @Override
+    public void handleUpdate(Update update) {
+        if (!update.hasMessage()) {
+            nextHandler.handleUpdate(update);
+            return;
+        }
+    }
+
+    @Override
+    public void setNext(UpdateHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+}
