44653620cc561640e739926f06eb9e63d1c539d5
itsyga <chel1997@mail.ru>
2024-08-05 20:21:57 +0500

    feat(controller): implement UpdateHandlers chain initialization in TelegramTicketBot class

--- src/main/java/ru/itsyga/telegramticketbot/controller/TelegramTicketBot.java
+++ src/main/java/ru/itsyga/telegramticketbot/controller/TelegramTicketBot.java
@@ -6,8 +6,25 @@ import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.List;
+
 @Component
 public class TelegramTicketBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+    private final UpdateHandler chainHead;
+
+    public TelegramTicketBot(List<UpdateHandler> handlers) {
+        if (handlers.isEmpty()) {
+            chainHead = new DefaultHandler();
+        } else {
+            chainHead = handlers.getFirst();
+            for (int i = 0; i < handlers.size(); i++) {
+                var current = handlers.get(i);
+                var next = i < handlers.size() - 1 ? handlers.get(i + 1) : new DefaultHandler();
+                current.setNext(next);
+            }
+        }
+    }
+
     @Override
     public String getBotToken() {
         return System.getenv("bot_token");
@@ -20,6 +37,6 @@ public class TelegramTicketBot implements SpringLongPollingBot, LongPollingSingl
 
     @Override
     public void consume(Update update) {
-
+        chainHead.handleUpdate(update);
     }
 }
