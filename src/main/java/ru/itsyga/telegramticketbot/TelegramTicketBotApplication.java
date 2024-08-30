0e72fffc8e7a70113c878f34584994750f48057b
itsyga <chel1997@mail.ru>
2024-08-30 11:18:04 +0500

    initial commit

---
+++ src/main/java/ru/itsyga/telegramticketbot/TelegramTicketBotApplication.java
@@ -0,0 +1,13 @@
+package ru.itsyga.telegramticketbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramTicketBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramTicketBotApplication.class, args);
+    }
+
+}
