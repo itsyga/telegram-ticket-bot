ed5364d660442fa9376ea92721031a79d5b986d5
itsyga <chel1997@mail.ru>
2024-09-07 20:48:47 +0500

    fix(director) add annotation @Component to EditMessageDirector, change edited message text

--- src/main/java/ru/itsyga/telegramticketbot/director/EditMessageDirector.java
+++ src/main/java/ru/itsyga/telegramticketbot/director/EditMessageDirector.java
@@ -1,13 +1,15 @@
 package ru.itsyga.telegramticketbot.director;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 
+@Component
 public class EditMessageDirector {
     public EditMessageText build(Long chatId, Integer msgId, String selectedData) {
         return EditMessageText.builder()
                 .chatId(chatId)
                 .messageId(msgId)
-                .text("Выбран " + selectedData)
+                .text("Вы выбрали " + selectedData)
                 .build();
     }
 }
