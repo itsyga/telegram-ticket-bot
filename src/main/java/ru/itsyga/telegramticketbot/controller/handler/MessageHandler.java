f50883cff9dee5b87de4518fcb36e4db6a72e727
itsyga <chel1997@mail.ru>
2024-09-07 15:17:35 +0500

    feat(controller) add MessageService dependency in MessageHandler

--- src/main/java/ru/itsyga/telegramticketbot/controller/handler/MessageHandler.java
+++ src/main/java/ru/itsyga/telegramticketbot/controller/handler/MessageHandler.java
@@ -1,14 +1,20 @@
 package ru.itsyga.telegramticketbot.controller.handler;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
+import ru.itsyga.telegramticketbot.service.MethodService;
 
 @Order(1)
 @Component
+@RequiredArgsConstructor
 public class MessageHandler implements UpdateHandler {
     private UpdateHandler nextHandler;
+    @Qualifier("messageService")
+    private final MethodService messageService;
 
     @Override
     public void handleUpdate(Update update) {
@@ -17,6 +23,7 @@ public class MessageHandler implements UpdateHandler {
             nextHandler.handleUpdate(update);
             return;
         }
+        messageService.serve(msg);
     }
 
     @Override
